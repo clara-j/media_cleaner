21111fd10ab90346c44fb7b54d67b90112d390e2
terrelsa13 <terrelsa13@gmail.com>
2020-09-26 19:55:46 -0400

    Update media_cleaner.py

--- media_cleaner.py
+++ media_cleaner.py
@@ -462,7 +462,7 @@ def get_items(server_url, user_key, auth_key):
                     print('\nError encountered - Keep Unknown Media Type: \n' + str(item))
             print(':[KEEPING UNKNOWN MEDIA TYPE] - ' + item_details)
 
-    if len(deleteItems) <= 0:
+    if len(data['Items']) <= 0:
         print('[NO WATCHED ITEMS]')
 
     print('-----------------------------------------------------------')
